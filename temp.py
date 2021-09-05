3ec6a948d2f148ebd04977bc9a51b121e4d9f65e
William <zizou.ee99@g2.nctu.edu.tw>
2021-09-05 08:05:09 +0800

    code

--- temp.py
+++ temp.py
@@ -1,22 +1,15 @@
-weight = float(input('weight : ?'))
-height = float(input('height : ?'))
-bmi = weight / ((height / 100)*(height / 100))
-print('bmi = ', bmi)
-
-if_end = input('if_end : ?')
-if if_end == 'if_end':
-	raise SystemExit
-
-if bmi < 18.5:
-	print('light')
-elif bmi >= 18.5 and bmi < 24:
-	print('fit')
-elif bmi >= 24 and bmi < 27:
-	print('overweight')
-elif bmi >= 27 and bmi < 30:
-	print('light fat')
-elif bmi >= 30 and bmi < 35:
-	print('medium fat')			
-else:
-	print('very fat')
- 
+import random 
+pw = 'a123456'
+cnt = 0;
+r = random.randint(1, 100)
+while True:
+	r_in = int(input('1~100 = ?'))
+	if r == r_in:
+		print('correct')
+		break 
+	else:
+		print('not correct')
+		if r > r_in:
+			print('random bit > ', r_in)
+		else:
+			print('random bit < ', r_in)			
